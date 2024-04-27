23799b79c78c852148f51b573111e3a3db2aa840
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2024-04-27 21:16:49 +0530

    configured Tab related settings

--- init.lua
+++ init.lua
@@ -908,5 +908,11 @@ require('lazy').setup({
   },
 })
 
+-- tab related settings
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
